e52a3254eb016770bcbde8ebcb91deb719039e1f
Tirth Patel <c0732052@mylambton.ca>
2019-04-18 00:04:06 -0400

    init

--- MultiplePageAssignment/MainActivity.cs
+++ MultiplePageAssignment/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
@@ -12,19 +13,47 @@ namespace MultiplePageAssignment
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        public static Spinner menuNames;
+        public static TextView ProductPriceTv, TotalPriceTv;
+        public static EditText Name;
+        public static double total;
+        public static Button btnadd, btnorder;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            double[] foodPriceArray = { 3, 8, 12, 2, 2.5, 4 };
+            ProductPriceTv = (TextView)FindViewById(Resource.Id.etprice);
+            TotalPriceTv = (TextView)FindViewById(Resource.Id.ettotal);
+            menuNames = (Spinner)FindViewById(Resource.Id.spinproduct);
+            btnadd = (Button)FindViewById(Resource.Id.btadd);
+            btnorder = (Button)FindViewById(Resource.Id.btorder);
+            Name = (EditText)FindViewById(Resource.Id.etname);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            var uNamesAdapter = ArrayAdapter.CreateFromResource(this, Resource.Array.foodArray, Android.Resource.Layout.SimpleSpinnerItem);
+            menuNames.Adapter = uNamesAdapter;
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            menuNames.ItemSelected += delegate
+
+            {
+                long i = menuNames.SelectedItemId;
+                ProductPriceTv.Text = foodPriceArray[i].ToString();
+                total = foodPriceArray[i];
+                Toast.MakeText(this, "The Selected Food is : " + menuNames.SelectedItem, ToastLength.Long).Show();
+            };
+
+            btnadd.Click += delegate
+
+            {
+                TotalPriceTv.Text = ProductPriceTv.Text;
+            };
+
+            btnorder.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(PlaceOrder));
+                StartActivity(intent);
+            };
         }
-
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
---
+++ MultiplePageAssignment/PlaceOrder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MultiplePageAssignment
+{
+    [Activity(Label = "PlaceOrder")]
+    public class PlaceOrder : Activity
+    {
+
+        public static Button BackBtn, FinalBtn;
+        public static TextView welcome,payment;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.page2);
+            // Create your application here
+            BackBtn = (Button)FindViewById(Resource.Id.btback);
+            FinalBtn = (Button)FindViewById(Resource.Id.btfinal);
+            welcome = (TextView)FindViewById(Resource.Id.etwelcome);
+            payment = (TextView)FindViewById(Resource.Id.etfinal);
+
+            welcome.Text = MainActivity.Name.Text;
+
+            FinalBtn.Click += delegate
+            {
+                double pay = MainActivity.total * 1.13;
+                payment.Text = pay.ToString();
+            };
+
+            BackBtn.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            };
+        }
+    }
+}
